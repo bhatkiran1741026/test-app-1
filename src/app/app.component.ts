9fddb9c585c3b8eb880b06138799b08d388050ed
bhatkiran74 <bhatkiran74@gmail.com>
2022-01-22 12:42:21 +0530

    adding bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'test-app-1';
+
+  // string interpolation
+
+  serverId: number = 3320;
+  serverName: string = 'https://localhost:3320/angular.io';
 }
